0d6b3ed7302eb7a1faf9313cdc31ccb15234e557
wmc2tokyo <wmcooper2@yahoo.com>
2020-05-25 23:51:10 +0900

    Started to rework my original tribute page with what I've learned over the past couple of years.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Data from "./data";
+// import rotationkey from "./rotationkey.jpg";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+const BulletPoints = (props) => {
+  console.log(props);
+  let bullets = Data.map((point, index) => {
+    return (
+      <React.Fragment key={index}>
+        <img src={point.img} className="rotationkey" alt={point.alt}></img>
+        <li>{point.data}</li>
+      </React.Fragment>
+    );
+  });
+  return bullets;
+};
+
+const displayScroll = () => {
+  console.log(window.pageXOffset);
+};
+
+class App extends React.Component {
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.handleScroll);
+  }
+  render() {
+    return (
+      <div
+        className="App"
+        onScroll={() => {
+          displayScroll();
+        }}
+      >
+        <header>John Keely</header>
+        <img src="portrait.jpg" id="portrait" alt="portrait"></img>
+        <p id="lifespan">Life to Death</p>
+        <p id="summary">
+          Keely is my favorite person in history. Although his work is marred
+          with claims of fraud and incomplete theories, I love those incomplete
+          ideas that he left behind; so much is left to one's imagination. His
+          research and thoughts about the vibratory nature of our reality is a
+          fantastic concept to consider. Here are some interesting things about
+          this man.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <ul>{BulletPoints(Data)}</ul>
+        <img
+          src="book.jpg"
+          alt="free energy pioneer book cover"
+          id="book"
+        ></img>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/data.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+const Data = [
+  {
+    data: "He was working on ideas that are echoed in modern quantum physics.",
+    img: "rotationkey.jpg",
+  },
+  {
+    data: "He thought that the human brain was a kind of resonant device.",
+    img: "rotationkey.jpg",
+  },
+
+  {
+    data:
+      "He believed in the existence of the ether which he estimated has a density close to 1 million times that of steel.",
+    img: "rotationkey.jpg",
+  },
+  {
+    data:
+      "He failed to make a business to sell a motor based on his ideas about resonance which angered his investors and further lower ed any credibility his ideas held during his time.",
+    img: "rotationkey.jpg",
+  },
+  {
+    data:
+      "Keely believed that a gallon of water had the latent energy stored within it to destroy the entire universe if released all at once much like how an atomic bomb releases its energy, just much further down than the atomic level.",
+    img: "rotationkey.jpg",
+  },
+  {
+    data:
+      <React.Fragment>His work is being revisited by <a href='http://www.svpwiki.com/Dale+Pond'>Dale Pond</a> who has been expanding on Keely's work while applying his own ideas</React.Fragment>,
+    img: "rotationkey.jpg",
+  },
+  {
+    data:
+      "Although nothing ever came of his ideas, and in spite of any religious influences on his work, I think he is the most interesting inventor/con-man/whatever.",
+    img: "rotationkey.jpg",
+  },
+];
+
+export default Data;
